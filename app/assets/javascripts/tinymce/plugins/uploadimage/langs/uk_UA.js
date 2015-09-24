6c6f9a95f8b55fea90b2c54946fc550ffbac631e
VasiliyNovosad <novosadvasiliy@gmail.com>
2015-09-24 16:35:13 +0300

    add devise uk localization

---
+++ app/assets/javascripts/tinymce/plugins/uploadimage/langs/uk_UA.js
@@ -0,0 +1,11 @@
+tinyMCE.addI18n('uk_UA', {
+  'Insert an image from your computer': "Вставити зображення із вашого комп'ютера",
+  'Insert image': "Вставити зображення",
+  'Choose an image': "Виберіть зображення",
+  'You must choose a file': "Ви повинні вибрати зображення",
+  'Got a bad response from the server': "Отримано хибну відповідь від сервера",
+  "Didn't get a response from the server": "Сервер не відповідає",
+  'Insert': "Вставити",
+  'Cancel': "Відміна",
+  'Image description': "Опис зображення",
+});
